37af05988cb6371fdf217782148930617e0dfeb8
Yngve Høiseth <yngve@hoiseth.net>
2019-10-22 20:22:29 +0200

    Prepare for deployment

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+
+from django_heroku import settings
+
 AUTH_USER_MODEL='app.User'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+settings(locals())
